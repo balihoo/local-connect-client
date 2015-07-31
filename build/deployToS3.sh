7f3923829f0fc509c39eb0fce973c37a944a9897
balihoo-soconnor <soconnor@balihoo.com>
2015-07-30 18:34:42 -0600

    modified script

--- build/deployToS3.sh
+++ build/deployToS3.sh
@@ -9,5 +9,5 @@ s3Url="s3://${S3BUCKET}/${FILE}"
 s3Url=$(echo "${s3Url}" | sed s/\ /_/g)
 
 putCmd="/usr/bin/s3cmd put --force --acl-public --config=${bamboo_dev_aws_s3cfg} ${FILE} ${s3Url}"
-echo "Putting archive to s3... cmd: $putCmd"
+echo "Putting file to s3... cmd: $putCmd"
 $putCmd
